fa048359b85099a79f558808f2c513e87903ee9e
Triin <triin.poll@gmail.com>
2025-08-05 22:56:04 +0300

    make game replayable

--- NumberGuessing/NumberGuessing.java
+++ NumberGuessing/NumberGuessing.java
@@ -1,49 +1,80 @@
 import java.util.Random;
 import java.util.Scanner;
 
-    /*
-     * add a "difficulty" setting — like easy (0–50), medium (0–100), hard (0–500) - OKIDOKI check
-     * 
-     * Make it replayable ? ?
-     * Add input range validation ??
-     * Format a bit more ???
-     */
-
 public class NumberGuessing {
     public static void main (String[] args){
 
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-        int userInput, counter=0, difficulty=0, number=1; // counter to keep count of user tries in the guessing
+        int userGuess, counter=0, difficulty=0, randNumber=1; // counter to keep count of user tries in the guessing
 
-        //difficulty setting
-        System.out.println("Choose a difficulty: ");
+        difficulty= menyChoise(difficulty, scanner);
+        randNumber = randomNumber(difficulty, randNumber, random);
+
+        //DELETO LATER. IS ONLY FOR CHECKINGG
+        System.out.println("CHECKCHEK NUMBAH  " + randNumber + " DIFFFF: " + difficulty);
+
+        //number guessing checker
+        while(true){
+            // 4. to end game / quit program
+            if(difficulty==4){
+                break;
+            }
+            
+            if(scanner.hasNextInt()){
+                userGuess = scanner.nextInt();
+                counter++;
+                
+                if (userGuess == randNumber){
+                    System.out.println("Correct! number is " + randNumber);
+                    System.out.println("You took " + counter + " tries");
+
+                    difficulty= menyChoise(difficulty, scanner);
+                    if(difficulty==4){
+                        break;
+                    } else{
+                        randNumber = randomNumber(difficulty, randNumber, random);
+                    }
+                } else {
+                    if(userGuess > randNumber){
+                        System.out.println("number is smaller than " + userGuess);
+                    } else{
+                        System.out.println("number is bigger than " + userGuess);
+                    }
+                }
+            } else {
+                System.out.println("Invalid input. please enter a number");
+                scanner.next();
+            }
+        }
+    }
+
+    //meny
+    public static int menyChoise(int difficulty,Scanner scanner){
+        System.out.println("\nChoose a difficulty: ");
         System.out.println("1. Easy (0-50)");
         System.out.println("2. Medium (0-100)");
         System.out.println("3. Hard (0-500)");
         System.out.println("4. Exit");
 
-        //looping,  only works 1-3 and checks if its numbah
         while(true){
             if(scanner.hasNextInt()){
                 difficulty = scanner.nextInt();
                 switch (difficulty) {
                     case 1:
-                        number = random.nextInt(51);
-                        System.out.println("\nGuess the number from 0 to " + 50);
+                        System.out.println("\nGuess the number from 0 to 50");
                         break;
                     case 2:
-                        number = random.nextInt(101);
-                        System.out.println("\nGuess the number from 0 to " + 100);
+                        System.out.println("\nGuess the number from 0 to 100");
                         break;
                     case 3:
-                        number = random.nextInt(501);
-                        System.out.println("\nGuess the number from 0 to " + 500);
+                        System.out.println("\nGuess the number from 0 to 500");
                         break;
                     case 4:
-                        System.out.println("BYEEEE! "); //change it later
+                        System.out.println("BYEEEE! ");
                         break;
                     default:
+                        //no work, needs to say invalid and choose 1-4 :( BLAME THE BREAK IN THE IF STATMENT!
                         break;
                 }
                 break;
@@ -52,38 +83,21 @@ public class NumberGuessing {
                 scanner.next();
             }
         }
+        return difficulty;
+    }
 
-        //DELETO LATER. IS ONLY FOR CHECKINGG
-        //System.out.println("CHECKCHEK NUMBAH  " + number + " DIFFFF: " + difficulty);
-
-
-        //number guessing checker
-        while(true){
-
-            //if user chose toquit 4. end game
-            if(difficulty==4){
+    public static int randomNumber(int difficulty, int number, Random random){
+        switch (difficulty) {
+            case 1:
+                number = random.nextInt(51);
                 break;
-            }
-            
-            if(scanner.hasNextInt()){
-                userInput = scanner.nextInt();
-                counter++;
-                
-                if (userInput == number){
-                    System.out.println("Correct! number is " + number);
-                    System.out.println("You took " + counter + " tries");
-                    break;
-                } else {
-                    if(userInput>number){
-                        System.out.println("number is smaller than " + userInput);
-                    } else{
-                        System.out.println("number is bigger than " + userInput);
-                    }
-                }
-            } else {
-                System.out.println("Invalid input. please enter a number");
-                scanner.next();
-            }
+            case 2:
+                number = random.nextInt(101);
+                break;
+            case 3:
+                number = random.nextInt(501);
+                break;
         }
+        return number;
     }
 }
--- NumberGuessing/README.md
+++ NumberGuessing/README.md
@@ -1,4 +1,12 @@
-#Simple Number Guessing Game
-very basic number Guessing Game: just guess a number from 0 to 100
-
+# Simple Number Guessing Game
+very basic number Guessing Game: just guess the number
 To practice loops, conditionals, input/output
+
+## Features
+1. Meny selection which also acts as difficulty setting
+2. user input
+3. input validation
+
+## For future add, perhaps
+1. Add input range validation
+2. See if possible to format better
